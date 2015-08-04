c729d64c630d23e967b6b393257fdfd8de64d4f7
Sven Baumann <baumann.sv@gmail.com>
2015-08-04 09:04:03 +0200

    add config file

---
+++ contao/config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * contao-calendar-filter
+ *
+ * Copyright (C) ContaoBlackForest
+ *
+ * @package   contao-calendar-filter
+ * @file      config.php
+ * @author    Sven Baumann <baumann.sv@gmail.com>
+ * @author    Dominik Tomasi <dominik.tomasi@gmail.com>
+ * @license   LGPL-3.0+
+ * @copyright Copyright 2015 ContaoBlackForest
+ */
