96df8a056b6ba381202727ad2d5c57b890d38e29
Sven Baumann <baumann.sv@gmail.com>
2015-08-06 04:21:24 +0200

    add fields to tl_module

--- contao/dca/tl_module.php
+++ contao/dca/tl_module.php
@@ -12,3 +12,20 @@
  * @license   LGPL-3.0+
  * @copyright Copyright 2015 ContaoBlackForest
  */
+
+echo "";
+
+$GLOBALS['TL_DCA']['tl_module']['palettes']['eventlist'] = str_replace('cal_calendar', 'cal_calendar,filterField', $GLOBALS['TL_DCA']['tl_module']['palettes']['eventlist']);
+
+$fields = array(
+    'filterField' => array(
+        'label'            => &$GLOBALS['TL_LANG']['tl_module']['cal_calendar'],
+        'exclude'          => true,
+        'inputType'        => 'checkboxWizard',
+        'options_callback' => array('ContaoBlackForest\Module\CalendarFilter\DataContainer\Module', 'getFilterFields'),
+        'eval'             => array('multiple' => true),
+        'sql'              => "blob NULL"
+    )
+);
+
+$GLOBALS['TL_DCA']['tl_module']['fields'] = array_merge($fields, $GLOBALS['TL_DCA']['tl_module']['fields']);
